46f3ef47096022bff1f8bf6f67656e341b30c881
Weverton <wevertonbezzera013@gmail.com>
2022-02-17 20:32:01 -0300

    final

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,33 +3,33 @@ import { Autocomplete } from '@react-google-maps/api';
 import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
 
-import useStyles from './styles.js'
+import useStyles from './styles.js';
 
-const Header = () =>{
-    const classes = useStyles();
+const Header = ({ onPlaceChanged, onLoad }) => {
+  const classes = useStyles();
 
-    return(
-        <AppBar position="static">
-        <Toolbar className={classes.toolbar}>
-          <Typography variant="h5" className={classes.title}>
-            Travel Advisor
+  return (
+    <AppBar position="static">
+      <Toolbar className={classes.toolbar}>
+        <Typography variant="h5" className={classes.title}>
+          Travel Advisor
+        </Typography>
+        <Box display="flex">
+          <Typography variant="h6" className={classes.title}>
+            Explore new places
           </Typography>
-          <Box display="flex">
-            <Typography variant="h6" className={classes.title}>
-              Explore new places
-            </Typography>
-            {/* <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}> */}
-              <div className={classes.search}>
-                <div className={classes.searchIcon}>
-                  <SearchIcon />
-                </div>
-                <InputBase placeholder="Search…" classes={{ root: classes.inputRoot, input: classes.inputInput }} />
+          <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
               </div>
-            {/* </Autocomplete> */}
-          </Box>
-        </Toolbar>
-      </AppBar>
-    )
-}
+              <InputBase placeholder="Search…" classes={{ root: classes.inputRoot, input: classes.inputInput }} />
+            </div>
+          </Autocomplete>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
 
 export default Header;
